a9ef16e63f8f5bbeac785404c2d97b9137a13e6f
JohannFonseca <arielfonseca049@gmail.com>
2024-05-08 10:09:02 -0600

    Inicio listo

--- Inicio.java
+++ Inicio.java
@@ -9,7 +9,7 @@ public class Inicio {
     public void mostrarVentana() {
         // Crear la ventana de inicio
         ventanaInicio = new JFrame("Inicio");
-        ventanaInicio.setSize(400, 300);
+        ventanaInicio.setSize(450, 450);
         ventanaInicio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Crear el panel principal con un diseño de cuadrícula
@@ -25,24 +25,35 @@ public class Inicio {
         JLabel labelNombre = new JLabel("Nombre:");
         JTextField txtNombre = new JTextField(20);
         panelPrincipal.add(labelNombre, gbc);
-        gbc.gridy++;
+        gbc.gridx++;
         panelPrincipal.add(txtNombre, gbc);
 
         // Etiqueta y campo de texto para la cédula
+        gbc.gridx = 0;
+        gbc.gridy++;
         JLabel labelCedula = new JLabel("Cédula:");
         JTextField txtCedula = new JTextField(20);
         panelPrincipal.add(labelCedula, gbc);
-        gbc.gridy++;
+        gbc.gridx++;
         panelPrincipal.add(txtCedula, gbc);
 
         // Etiqueta y campo de texto para el lugar de procedencia
+        gbc.gridx = 0;
+        gbc.gridy++;
         JLabel labelProcedencia = new JLabel("Lugar de Procedencia:");
         JTextField txtProcedencia = new JTextField(20);
         panelPrincipal.add(labelProcedencia, gbc);
-        gbc.gridy++;
+        gbc.gridx++;
         panelPrincipal.add(txtProcedencia, gbc);
 
+        // Espacio en blanco para separar los campos de texto de los botones
+        gbc.gridy++;
+        gbc.gridx = 0;
+        gbc.gridwidth = 2;
+        panelPrincipal.add(Box.createVerticalStrut(20), gbc);
+
         // Botón para aceptar los datos ingresados
+        gbc.gridy++;
         JButton btnAceptar = new JButton("Siguiente");
         btnAceptar.setPreferredSize(new Dimension(100, 30));
         btnAceptar.addActionListener(e -> {
@@ -63,6 +74,7 @@ public class Inicio {
         panelPrincipal.add(btnAceptar, gbc);
 
         // Botón para regresar a la ventana de bienvenida
+        gbc.gridy++;
         JButton btnRegresar = new JButton("Regresar");
         btnRegresar.setPreferredSize(new Dimension(100, 30));
         btnRegresar.addActionListener(e -> {
@@ -70,14 +82,7 @@ public class Inicio {
             bienvenida.main(null); // Mostrar la ventana de bienvenida
             ventanaInicio.dispose(); // Cerrar la ventana de inicio
         });
-        gbc.gridx++;
         panelPrincipal.add(btnRegresar, gbc);
-
-        // Agregar espacio en blanco en la parte inferior para una mejor apariencia
-        gbc.gridy++;
-        gbc.gridx = 0;
-        gbc.gridwidth = 2;
-        panelPrincipal.add(Box.createVerticalStrut(30), gbc);
 
         // Agregar el panel principal a la ventana
         ventanaInicio.add(panelPrincipal);
